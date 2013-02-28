af03df6b86d731ab3f3f87531f1e082c7b6fb5d3
Kim Leo <kenpusney@gmail.com>
2013-02-28 13:17:40 +0800

    smarter install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-[ -d /usr/lib/kit ] || mkdir /usr/lib/kit
+get_konf(){
+    rm -rf konfig && curl https://raw.github.com/kenpusney/konfigurations/master/konfig > konfig
+}
 
-cp konfig /usr/lib/kit
+inst_konf(){
+    [ -f ./konfig ] || get_konf
+    [ -d /usr/lib/kit ] || mkdir /usr/lib/kit &&  cp konfig /usr/lib/kit
+    if [ -e /usr/bin/konfig ]
+    then
+        echo "Remove old version ..."
+        rm -rf /usr/bin/konfig
+    fi
+    ln -s /usr/lib/kit/konfig /usr/bin/konfig 
+}
 
-if [ -e /usr/bin/konfig ]
-then
-    rm -rf /usr/bin/konfig
-fi
-ln -s /usr/lib/kit/konfig /usr/bin/konfig
+[ $# -gt 0 ] && case $1 in
+    "uninstall" | "un" )
+        rm -rf /usr/bin/konfig && rm -rf /usr/lib/kit 
+        echo "Uninstall successfully!" && exit ;;
+    "dev" ) 
+        get_konf && inst_konf && exit ;;
+    * )
+        echo "Unknown command."
+        exit 1;;
+esac
 
-
+inst_konf
